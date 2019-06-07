b69f66a5a0468246d4d74ab266b42f691778e2b7
bhavyamehta022 <bhavyamehta022@gmail.com>
2019-06-07 08:06:09 -0800

    multilanguage

--- app/src/main/java/com/bhavya/myapplication/MainActivity.java
+++ app/src/main/java/com/bhavya/myapplication/MainActivity.java
@@ -1,23 +1,35 @@
 package com.bhavya.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,14 +45,63 @@ public class MainActivity extends AppCompatActivity {
     EditText mobile;
     EditText pick;
     EditText drop;
-
+    EditText submit;
+    EditText sub;
+    EditText email;
+    EditText password;
+    EditText register_email;
+    EditText register_password;
+    EditText register_name;
+    String currentLanguage="en",currentLang;
+    Locale myLocale;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         checkDay();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //todo in manifest, i have added soft input mode , still the screen is not scrolling
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
+
+        currentLanguage = getIntent().getStringExtra(currentLang);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        List<String> list = new ArrayList<String>();
+
+        list.add("Select language");
+        list.add("English");
+        list.add("Hindi");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                switch (position) {
+                    case 0:
+                        break;
+                    case 1:
+                        setLocale("en");
+                        break;
+
+                    case 2:
+                        setLocale("hi");
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
     }
+
+
+
     public void open_cc(View view)
     {
         ParliamentConstituency parliamentConstituency=new ParliamentConstituency();
@@ -127,6 +188,7 @@ public class MainActivity extends AppCompatActivity {
             linear.setVisibility(View.VISIBLE);
         }
 
+
     }
     public void open_feedback(View view)
     {
@@ -136,7 +198,7 @@ public class MainActivity extends AppCompatActivity {
     }
     public void submit(View view)
     {
-        EditText submit=findViewById(R.id.otp);
+        submit=findViewById(R.id.otp);
         String s=submit.getText().toString();
         if(s.length()!=0&&s.matches("[0-9]+"))
         {
@@ -147,9 +209,10 @@ public class MainActivity extends AppCompatActivity {
         {
             Toast.makeText(this,"Enter a valid OTP",Toast.LENGTH_SHORT).show();
         }
+
     }
-public void open_after()
-{
+    public void open_after()
+    {
     AfterFragment afterFragment = new AfterFragment();
     getSupportFragmentManager().beginTransaction().replace(R.id.container, afterFragment).commit();
 }
@@ -224,7 +287,7 @@ public void open_after()
 
     public void submitFeedback(View view)
     {
-        EditText sub=findViewById(R.id.edit1);
+        sub=findViewById(R.id.edit1);
         String s=sub.getText().toString();
         if(s.length()==0)
         {
@@ -237,8 +300,8 @@ public void open_after()
     }
     public void toastLogin(View v)
     {
-        EditText email=findViewById(R.id.edit1);
-        EditText password=findViewById(R.id.edit2);
+        email=findViewById(R.id.edit1);
+        password=findViewById(R.id.edit2);
         String email1=email.getText().toString();
         String pass=password.getText().toString();
         if(email1.length()==0&&pass.length()==0)
@@ -253,21 +316,22 @@ public void open_after()
     public void login(View v)
     {
         Login login=new Login();
-        getSupportFragmentManager().beginTransaction().replace(R.id.container,login).addToBackStack("tag").commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,login).addToBackStack("login").commit();
     }
     public void register(View v)
     {
         Register register=new Register();
-        getSupportFragmentManager().beginTransaction().replace(R.id.container,register).addToBackStack("tag").commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,register).addToBackStack("login").commit();
     }
     public void toastRegister(View v)
     {
-        EditText email=findViewById(R.id.reg_email);
-        EditText password=findViewById(R.id.reg_pass);
-        EditText name=findViewById(R.id.name);
-        String email1=email.getText().toString();
-        String pass=password.getText().toString();
-        String names=name.getText().toString();
+        //todo after clicking it, sometimes feedback is opened
+        register_email=findViewById(R.id.reg_email);
+        register_password=findViewById(R.id.reg_pass);
+        register_name=findViewById(R.id.name);
+        String email1=register_email.getText().toString();
+        String pass=register_password.getText().toString();
+        String names=register_name.getText().toString();
         if(email1.length()==0&&pass.length()==0&&names.length()==0)
         {
             Toast.makeText(this,"Enter the necessary details correctly!",Toast.LENGTH_SHORT).show();
@@ -277,4 +341,25 @@ public void open_after()
             Toast.makeText(this,"Registration successful!",Toast.LENGTH_SHORT).show();
         }
     }
+//todo rating numstars
+
+    public void setLocale(String localeName)
+    {
+
+            myLocale = new Locale(localeName);
+            Resources res = getResources();
+            DisplayMetrics dm = res.getDisplayMetrics();
+            Configuration conf = res.getConfiguration();
+            conf.locale = myLocale;
+            res.updateConfiguration(conf, dm);
+            Intent refresh = new Intent(this, MainActivity.class);
+            refresh.putExtra(currentLang, localeName);
+            startActivity(refresh);
+
+
+    }
+
+
+
 }
+
